73b7ff500aba29b5e8a9e232c9b21fa596af5bb7
yunjun-yeong <ypjun100@gmail.com>
2019-08-27 21:25:53 +0900

    싱크가 안맞던 문제 수정

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 import profane_recognizer
 import threading
 
-input_video = 'input/test2.mp4'
+input_video = 'input/test0.mp4'
 
 if __name__ == '__main__':
     print('Reset all data files')
@@ -33,19 +33,19 @@ if __name__ == '__main__':
         task0[i].join()
 
     print('Second Step')
-    # task1 = [None] * len(audio_infor_1)
-    # for i, val in enumerate(audio_infor_1):
-    #     task1[i] = threading.Thread(target=profane_recognizer.get_profane_time, args=(1, i, val[0], val[1], val[2]))
-    #     task1[i].start()
-    #
-    # for i, _ in enumerate(audio_infor_1):
-    #     print(str(i + 1) + ' / ' + str(len(audio_infor_1)))
-    #     task1[i].join()
+    task1 = [None] * len(audio_infor_1)
+    for i, val in enumerate(audio_infor_1):
+        task1[i] = threading.Thread(target=profane_recognizer.get_profane_time, args=(1, i, val[0], val[1], val[2]))
+        task1[i].start()
+
+    for i, _ in enumerate(audio_infor_1):
+        print(str(i + 1) + ' / ' + str(len(audio_infor_1)))
+        task1[i].join()
 
     #Get result from txt files in result_detector folder
     print('Load Result')
     result = utils.get_all_data()
-
+    print(result)
     print('Generate sound')
     utils.generate_sound(result)
 
